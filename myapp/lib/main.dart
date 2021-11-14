7576ab63f5fbfcd8ba114521a3451f6c576df989
ShekAdnan <adnansk418@gmail.com>
2021-11-15 00:08:53 +0600

    variable declartion and initialization

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -4,8 +4,8 @@
  }
 
 */
-
-void main() {
+/*
+void main() { 
   // 01, 20, 40, 50, 1050 = non-fractional = int
 
   //10.4, 1.0, 250.45 = fractional = double
@@ -15,11 +15,45 @@ void main() {
   String name = 'My name is Mr Mango';
   print(name);
 
-/*
 
-Dhaka  is the capital of Bangladesh?
 
-a) true
-b) false
-*/
+// Dhaka  is the capital of Bangladesh?  
+
+// a) true //Boolean data types = true, false
+// b) false
+
 }
+*/
+
+// This is single line comment
+
+/* = its called forward slash and asterisk sign
+Multiline Comment
+
+Comments are a set of statements that are not executed by the compiler.
+
+The use of comments makes it easy for humans to understand the source code.
+*/
+
+// A variable is  'a named space in the memory' that stores values. In  other words, It acts a container for values in a program. Variable names are called identifiers.
+/*
+Example:
+var name = 'Adam';
+String named = 'Smon';
+int num = 10;
+double pi = 3.14;
+bool isWorked = true;
+*/
+
+// void main() {
+//   int age; // variable declaration
+//   age = 20; // variable initialization
+
+//   int sum = 25; // vaiable decalaration and initialization at the same time
+
+//   double num = 20.5;
+
+//   String name = 'Seheik h';
+
+//   bool isLoading = true;
+// }
