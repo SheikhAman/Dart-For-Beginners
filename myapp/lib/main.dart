0412dd1f48d128695eb16558f0a9ad1f7404082f
ShekAdnan <adnansk418@gmail.com>
2021-11-14 23:44:44 +0600

    data types

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,3 +1,25 @@
+/*
+ void main() { // main function //first program
+  print('Hello Dhaka'); // print function 
+ }
+
+*/
+
 void main() {
-  print('Hello Dhaka');
+  // 01, 20, 40, 50, 1050 = non-fractional = int
+
+  //10.4, 1.0, 250.45 = fractional = double
+
+  // A, B,  C, Apple, Orange = String represents a sequence of characters
+
+  String name = 'My name is Mr Mango';
+  print(name);
+
+/*
+
+Dhaka  is the capital of Bangladesh?
+
+a) true
+b) false
+*/
 }
